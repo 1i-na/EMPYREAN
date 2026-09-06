ddd6677a076ff55d8c57afeebe37d2bc9f8bdefb
1ina <lina.bounafaa@icloud.com>
2026-09-06 00:08:41 -0400

    order db.py into functions (get single user, get all users, create table, etc)

--- guild/db.py
+++ guild/db.py
@@ -1,21 +1,13 @@
 import sqlite3
 
+# database connection
 def get_conn():
     return sqlite3.connect('rg.db')
 
-def get_users():
-    conn = get_conn()
-
-    users = conn.execute('''
-        SELECT * FROM users
-        ORDER BY total_points DESC  
-    ''').fetchall()
-
-    conn.close()
-    return users
+conn = get_conn()
 
 # users table
-get_conn().execute('''
+conn.execute('''
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         username VARCHAR(255) NOT NULL,
@@ -28,6 +20,54 @@ get_conn().execute('''
         CONSTRAINT unique_rider UNIQUE (username, section)
     )
 ''')
+
+conn.commit()
+conn.close()
+
+# create user
+def create_user(username, password, section, rank):
+    global conn
+    try:
+        conn.execute('''
+            INSERT INTO users (
+                username, password, section, 
+                rank, total_points, 
+                bounties_completed, bounties_requested
+            ) VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            username, password, section, rank, 0, 0, 0
+        ))
+
+        conn.commit()
+        return True
+    except:
+        return False
+    finally:
+        conn.close()
+
+# get all users
+def get_users():
+    global conn
+
+    users = conn.execute('''
+        SELECT * FROM users
+        ORDER BY total_points DESC  
+    ''').fetchall()
+
+    conn.close()
+    return users
+
+# get specific user
+def get_user(username, section):
+    global conn
+
+    users = conn.execute('''
+        SELECT * FROM users
+        WHERE username = ? AND section = ?
+    ''', (username, section)).fetchone()
+
+    conn.close()
+    return user
 
 # dummy user
 try:
